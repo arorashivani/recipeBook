0a94e77ce16eb960556091b0e9133119e1568183
Shivani Arora <shivani.arora@clairvolex.local>
2016-09-15 09:51:22 +0530

    First commit

---
+++ collections/Recipes.js
@@ -0,0 +1,40 @@
+Recipes = new Meteor.Collection('recipes');
+
+Recipes.allow({
+  insert : function(userId, doc) {
+    return !!userId;
+  }
+});
+
+RecipeSchema = new SimpleSchema({
+  name:{
+    type:String,
+    label:"Name"
+  },
+  desc:{
+    type:String,
+    label:"Description"
+  },
+  author:{
+    type:String,
+    label:"Author",
+    autoValue:function () {
+      return this.userId
+    },
+    autoform:{
+      type:"hidden"
+    }
+  },
+  createdAt:{
+    type : Date,
+    label:"Created At",
+    autoValue:function () {
+      return new Date()
+    },
+    autoform:{
+      type:"hidden"
+    }
+  }
+});
+
+Recipes.attachSchema(RecipeSchema)
